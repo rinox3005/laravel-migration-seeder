204b5b3b3fedd79dc5027098bd641140bfa979ee
rinox3005 <peppe.rinox@gmail.com>
2024-07-09 14:05:17 +0200

    [UPD] created db_train_station and migration to create trains table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,14 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>Laravel Template</title>
+        <title>Trains</title>
         @vite("resources/js/app.js")
     </head>
 
     <body>
         <main>
             <section class="container my-5">
-                <h1 class="text-center">Laravel Template</h1>
+                <h1 class="text-center">Trains</h1>
                 <div class="d-flex justify-content-center m-5">
                     <img
                         src="{{ Vite::asset("resources/img/duck.jpg") }}"
