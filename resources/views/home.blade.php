0dee7be792201f6c10277430ed589ec2ad447437
rinox3005 <peppe.rinox@gmail.com>
2024-07-10 17:21:36 +0200

    [FIX] fixed wrong card display in home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,34 +10,35 @@
     <!-- Main Content -->
     <div class="container my-5">
         <div class="row g-4 justify-content-center">
-            <div
-                class="card col-lg-4 col-sm-6 bg-body-tertiary h-100 home-card p-3"
-            >
-                <h3>Book Tickets</h3>
-                <p>
-                    Book your tickets conveniently online and enjoy a
-                    hassle-free journey.
-                </p>
-                <a href="#" class="btn btn-danger">Book Now</a>
+            <div class="col-lg-4 col-sm-6">
+                <div class="card p-3">
+                    <h3>Book Tickets</h3>
+                    <p>
+                        Book your tickets conveniently online and enjoy a
+                        hassle-free journey.
+                    </p>
+                    <a href="#" class="btn btn-danger">Book Now</a>
+                </div>
             </div>
-            <div
-                class="card col-lg-4 col-sm-6 bg-body-tertiary h-100 home-card p-3"
-            >
-                <h3>Train Schedules</h3>
-                <p>
-                    Check the latest train schedules and plan your trip
-                    accordingly.
-                </p>
-                <a href="/trains" class="btn btn-danger">View Schedule</a>
+            <div class="col-lg-4 col-sm-6">
+                <div class="card p-3">
+                    <h3>Train Schedules</h3>
+                    <p>
+                        Check the latest train schedules and plan your trip
+                        accordingly.
+                    </p>
+                    <a href="/trains" class="btn btn-danger">View Schedule</a>
+                </div>
             </div>
-            <div
-                class="card col-lg-4 col-sm-6 bg-body-tertiary h-100 home-card p-3"
-            >
-                <h3>Contact Us</h3>
-                <p>
-                    Have questions or need assistance? Contact our support team.
-                </p>
-                <a href="#" class="btn btn-danger">Get in Touch</a>
+            <div class="col-lg-4 col-sm-6">
+                <div class="card p-3">
+                    <h3>Contact Us</h3>
+                    <p>
+                        Have questions or need assistance? Contact our support
+                        team.
+                    </p>
+                    <a href="#" class="btn btn-danger">Get in Touch</a>
+                </div>
             </div>
         </div>
     </div>
